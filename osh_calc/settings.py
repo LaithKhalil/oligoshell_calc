ad6e949a9bf2da7c8347993da5bc2c3f8a820225
LaithKhalil <laithik@gmail.com>
2025-08-31 19:14:58 -0400

    Update settings.py

--- osh_calc/settings.py
+++ osh_calc/settings.py
@@ -23,7 +23,7 @@ load_dotenv(path_to_env_file)
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_USER_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'dev-insecure-fallback')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
